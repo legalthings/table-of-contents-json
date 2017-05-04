7748ea08cd9902130402ccc46da75a4395137fd8
Moesjarraf J <moesjarraf@gmail.com>
2017-05-04 15:06:02 +0200

    Fixup for generating toc for empty headers

--- src/table-of-contents-json.js
+++ src/table-of-contents-json.js
@@ -2,6 +2,7 @@
 
 const cheerio = require('cheerio');
 const beautifyHtml = require('js-beautify').html;
+const minify = require('html-minifier').minify;
 
 class TableOfContentsJSON {
     constructor() {
@@ -24,21 +25,31 @@ class TableOfContentsJSON {
 
         this.options = Object.assign({}, this.defaultOptions, options);
 
-        let $ = cheerio.load(html);
-        let $headers = $(':header');
-        let list = [];
+        // we minify because cheerio seems to have trouble finding children if they aren't on one line
+        const minifiedHtml = minify(html, {collapseWhitespace: true});
+        const $ = cheerio.load(minifiedHtml);
+        const $headers = $(':header');
+        const list = [];
 
         for (let i = this.options.skipHeader; i < $headers.length; i++) {
-            if ($headers[i].children.length > 0) {
-                let name = $headers[i].children[0].data;
-                let type = $headers[i].name;
-                let headerSize = type.charAt(1);
-                let node = {id: i, name: name, type: type, children: []};
-                list.push(node);
+            if (!$headers[i].children.length) {
+                continue;
             }
+
+            const name = this.findNodeName($headers[i]);
+
+            if (!name || typeof name !== 'string' || !name.trim()) {
+                // do not add empty or corrupt headers to table of contents
+                continue;
+            }
+
+            const type = $headers[i].name;
+            const headerSize = type.charAt(1);
+            const node = {id: i, name: name, type: type, children: []};
+            list.push(node);
         }
 
-        let tree = [];
+        const tree = [];
 
         for (let i = 0; i < list.length; i++) {
             list[i].children = this.getChildrenJSON(list, list[i]);
@@ -60,11 +71,11 @@ class TableOfContentsJSON {
      * @return {array}
      */
     getChildrenJSON (list, node) {
-        let children = [];
-        let nodeHeaderSize = parseInt(node.type.charAt(1));
+        const children = [];
+        const nodeHeaderSize = parseInt(node.type.charAt(1));
 
         for (let i = 0; i < list.length; i++) {
-            let listHeaderSize = parseInt(list[i].type.charAt(1));
+            const listHeaderSize = parseInt(list[i].type.charAt(1));
 
             // add every immediate smaller header as a child of the current node
             if (listHeaderSize === nodeHeaderSize + 1 && list[i].id > node.id) {
@@ -78,6 +89,29 @@ class TableOfContentsJSON {
         }
 
         return children;
+    }
+
+    /**
+     * Find the name of a node
+     * This looks at only the first child of every nested node
+     * Needed in the case of for example <h1><b>Bold header</b></h1>
+     *
+     * @protected
+     * @param {object} node
+     * @return {string|null}
+     */
+    findNodeName (node) {
+        if (!node.children.length) {
+            return;
+        }
+
+        const name = node.children[0].data;
+
+        if (!name && node.children[0].children.length) {
+            return this.findNodeName(node.children[0])
+        }
+
+        return name;
     }
 
 
@@ -97,13 +131,13 @@ class TableOfContentsJSON {
 
         this.options = Object.assign({}, this.defaultOptions, options);
 
-        let html = base || '<html><head></head><body>[ table of contents ]</body></html>';
-        let $ol = cheerio.load('<ol id="toc"></ol>');
-        let style = '<style>#toc li,ol,ul { list-style: none; }</style>';
+        const html = base || '<html><head></head><body>[ table of contents ]</body></html>';
+        const $ol = cheerio.load('<ol id="toc"></ol>');
+        const style = '<style>#toc li,ol,ul { list-style: none; }</style>';
 
         for (let i = 0; i < json.length; i++) {
-            let node = json[i];
-            let $li = cheerio.load(`<li>${node.name}</li>`)('li');
+            const node = json[i];
+            const $li = cheerio.load(`<li>${node.name}</li>`)('li');
 
             if (node.children.length) {
                 this.addChildrenHTML($li, node.children);
@@ -117,7 +151,7 @@ class TableOfContentsJSON {
             toc = `<h1>${options.tocHeader}</h1>${toc}`;
         }
 
-        let $html = cheerio.load(html);
+        const $html = cheerio.load(html);
         $html('head').append(style);
         let result = $html.html();
         result = result.replace(/\[ table of contents \]/, toc);
@@ -136,7 +170,7 @@ class TableOfContentsJSON {
      * @return {object}
      */
     addChildrenHTML ($element, children) {
-        let $ol = cheerio.load(`<ol></ol>`)('ol');
+        const $ol = cheerio.load(`<ol></ol>`)('ol');
 
         for (let i = 0; i < children.length; i++) {
             let node = children[i];
